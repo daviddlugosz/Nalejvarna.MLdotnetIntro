07db79365a725fe1d664c45634137650de5a3fd6
David Długosz <david.dlugosz@hotmail.com>
2024-04-07 16:11:45 +0200

    Removed ReviewSearchResult

--- Nalejvarna.MLdotnetIntro.Shared/Contracts/ReviewSearchResult.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Nalejvarna.MLdotnetIntro.Shared.Contracts;
-
-public class ReviewSearchResult
-{
-    public string MovieTitle { get; set; }
-    public string Review { get; set; }
-    public int Rating { get; set; }
-}
